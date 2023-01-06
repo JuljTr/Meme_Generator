faebefe4d590479fb164789b50d387f18fe589d1
Juljeta Troka <juliet.troka@gmail.com>
2023-01-06 21:32:39 +0000

    added function to generate random meme

--- my-app/src/Components/Meme.js
+++ my-app/src/Components/Meme.js
@@ -1,13 +1,22 @@
+import memesData from '../memesData'
 export default function Meme(){
+    function getnewMeme(){
+        console.log("I was clicked")
+        const memesArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[randomNumber].url
+        console.log(randomNumber);
+        console.log(url);
+    }
     return(
         <div>
-            <form className="form">
+            <div className="form">
                 <input type="text" placeholder="Top Text" className="form-input"/>
                 <input type="text" placeholder="Bottom Text" className="form-input"/>
-                <button className="form-button">
-                    Get a new meme 
+                <button onClick={getnewMeme} className="form-button">
+                    Get a new meme image
                 </button>
-            </form>
+            </div>
         </div>
     )
 }
